6349beb6905dff0952258e78b65f786188728e9a
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-04-01 11:55:18 +0900

    計算できた。

--- main.rb
+++ main.rb
@@ -51,16 +51,30 @@ def exps
     f.puts( "def exps(a,b,c,d)" )
     f.puts( "  [" )
     m.values.each do |e| 
-      f.puts "    " + e.max_by{ |x| x.count("+") + x.count("*") - x.count("-") + x.count("/")  }+","
+      f.puts "    (" + e.max_by{ |x| x.count("+") + x.count("*") - x.count("-") + x.count("/")  }+".abs rescue nil),"
     end
-    f.puts( "  ]" )
+    f.puts( "  ].compact" )
     f.puts( "end" )
   end
   pp m.size
 end
 
+def score(n)
+  nums = exps(*n).select{ |x| x==x.to_i }
+  (0...).find{ |x| !nums.index(x) }
+end
 
+def main
+  scores=[]
+  [*0..9].repeated_combination(4) do |n|
+    scores.push( [score(n), n] )
+  end
+  pp scores.sort.reverse.take(10)
+  p exps(1,2,6,7).select{ |x| x==x.to_i }.sort.uniq
+  p exps(5,6,8,9).select{ |x| x==x.to_i }.sort.uniq
+end
 
 exps
 require "./exps.rb"
+main
 
